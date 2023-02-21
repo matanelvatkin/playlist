893e533f1f6326dfa8948c71ccd90e5efd444960
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 17:25:32 +0200

    client 1.2.2

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:5000/api/"
-axios.defaults.baseURL = "https://playlist-server.onrender.com"
+axios.defaults.baseURL = "https://playlist-server.onrender.com/api/"
 
 const apiCalls = async (method, url, data) => {
     axios.defaults.headers.common.Authorization = `Bearer ${localStorage.token}`
